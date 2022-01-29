17401e53e395ba048b188a43c7a15d614fe47c15
JiaaaJiaa <88329635+JiaaaJiaa@users.noreply.github.com>
2022-01-30 00:39:33 +0800

    gui_v5
    - Fixing Coord

--- java/com/example/geometrygui/HelloController.java
+++ java/com/example/geometrygui/HelloController.java
@@ -1,21 +1,23 @@
 package com.example.geometrygui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
-public class HelloController {
+public class HelloController implements Initializable {
 
     @FXML
     Button ruleButton, backButton,startButton;
@@ -27,6 +29,8 @@ public class HelloController {
     private Button confirmPoint, homeButton;
     @FXML
     private ImageView imgSuccess, BckgroundView;
+    @FXML
+    private ChoiceBox x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x31,y31,x32,y32,x33,y33;
 
     int points;
 
@@ -93,16 +97,6 @@ public class HelloController {
         window.setScene(new Scene(fxmlLoader.load(), 600, 400));
     }
 
-    //INSERT COORDINATES
-    public void enterCoordx(ActionEvent actionEvent) {
-        //Validation
-        //There is a label with #errorMSG
-    }
-
-    public void enterCoordy(ActionEvent actionEvent) {
-        //Validation
-        //There is a label with #errorMSG
-    }
 
     //Connecting Back to homepage
     public void GoToHome(ActionEvent actionEvent) throws IOException {
@@ -110,5 +104,28 @@ public class HelloController {
 
         Stage window = (Stage)homeButton.getScene().getWindow();
         window.setScene(new Scene(fxmlLoader.load(), 600, 400));
+    }
+
+    //Setting ChoiceBox
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+//        x1.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        y1.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        x2.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        y2.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        x3.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        y3.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        x4.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        y4.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        x5.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+//        y5.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+        x31.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+        y31.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+        x32.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+        y32.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+        x33.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+        y33.getItems().addAll(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,10);
+
     }
 }
